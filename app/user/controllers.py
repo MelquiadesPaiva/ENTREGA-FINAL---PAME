2751b64d2ae24b4ad69f09648669568f6e1e57bf
melquiadespaiva <melquiades.paiva@poli.ufrj.br>
2020-07-22 23:45:33 -0300

    adciona metodo de pagamento

--- app/user/controllers.py
+++ app/user/controllers.py
@@ -3,10 +3,92 @@ from flask_jwt_extended import jwt_required, create_access_token, decode_token
 from ..extensions import db
 from ..models import User
 import bcrypt
+# from flask import Flask, render_template, url_for, request, abort #relacao ao metodo de pagamento
+# import stripe #relacao ao metodo de pagamento
+
+
 
 
 user_api = Blueprint('user_api', __name__)
 
+'''
+
+#METODO DE PAGAMENTO, AINDA ESTA INCOMPLETO. PRECISA ALINHAR ALGUMAS COISAS COM O FRONT TB
+
+
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51H7W0FJWsyvl50pz89WI2VTXGEjTqnExVt7HFEPlbSxsly0LOJPtpQG5UbgmDjcDxucPBPkwm6GlMBSSwtFdHS4v00WciCiWj2'
+app.config['STRIPE_SECRET_KEY'] = 'sk_test_51H7W0FJWsyvl50pzzqQiKxVn2fV41e1otVJbyYdvnVIlZYq9eSEV1rrenrEZn9tt9jBdpzQKP5gwdbm14mDDkBes004ydJJ7lW'
+
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
+
+@app.route('/')
+def index():
+    
+   
+    
+    return render_template(
+        'index.html', 
+        #checkout_session_id=session['id'], 
+        #checkout_public_key=app.config['STRIPE_PUBLIC_KEY']
+    )
+
+@app.route('/stripe_pay')
+def stripe_pay():
+     
+     session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1H7WaFJWsyvl50pzLQlUKlmQ',
+            'quantity': 2,
+        }],
+        mode='payment',
+        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('index', _external=True),
+    )
+    
+    return {
+        'checkout_session_id': session['id'], 
+        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
+    }
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+@app.route('/stripe_webhook', methods=['POST'])
+def stripe_webhook():
+    print('WEBHOOK CALLED')
+
+    if request.content_length > 1024 * 1024:
+        print('REQUEST MUITO GRANDE')
+        abort(400)
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = 'whsec_Xj8wBk2qiUcjDEmYu5kfkkorJCJ5UUjw'
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('INVALID PAYLOAD')
+        return {}, 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('INVALID SIGNATURE')
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
+        print(line_items['data'][0]['description'])
+
+    return {}    
+'''
 
 @user_api.route('/users/login/', methods=['POST'])
 def login():
